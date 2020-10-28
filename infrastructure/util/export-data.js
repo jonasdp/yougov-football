23d568ada74d312db1324374a33be14a9ea28bd6
Jonas Persson <jonas.d.p@gmail.com>
2020-10-28 14:24:10 +0100

    Added jest test config to git

--- infrastructure/util/export-data.js
+++ infrastructure/util/export-data.js
@@ -1,40 +1,40 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const AWS = require('aws-sdk');
+const fs = require('fs')
+const AWS = require('aws-sdk')
 
-const fileName = '../data/yougov-football-data.json';
+const fileName = '../data/yougov-football-data.json'
 
-const credentials = new AWS.SharedIniFileCredentials({profile: 'jonasdp-aws-main--jonas'});
+const credentials = new AWS.SharedIniFileCredentials({ profile: 'jonasdp-aws-main--jonas' })
 
 AWS.config = new AWS.Config({
   credentials: credentials,
   region: 'eu-west-1'
-});
+})
 
-const dynamodb = new AWS.DynamoDB();
+const dynamodb = new AWS.DynamoDB()
 
 fs.readFile(fileName, (err, data) => {
-    if (err) throw err;
-    let jsonData = JSON.parse(data);
+  if (err) throw err
+  let jsonData = JSON.parse(data)
 
-    jsonData.forEach((element) => {
-      let putItemParams = {
-        Item: {
-         "name": {
-           S: element.name
-          }, 
-         "img": {
-           S: element.img
-          }
-        }, 
-        ReturnConsumedCapacity: "TOTAL",
-        TableName: "yougovFootballTeams"
-       };
+  jsonData.forEach((element) => {
+    let putItemParams = {
+      Item: {
+        name: {
+          S: element.name
+        },
+        img: {
+          S: element.img
+        }
+      },
+      ReturnConsumedCapacity: 'TOTAL',
+      TableName: 'yougovFootballTeams'
+    }
 
-       dynamodb.putItem(putItemParams, function(err, data) {
-        if (err) console.log(err, err.stack);
-        else     console.log(data);
-       });
+    dynamodb.putItem(putItemParams, function (err, data) {
+      if (err) console.log(err, err.stack)
+      else console.log(data)
     })
-});
+  })
+})
---
+++ src/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
+}
